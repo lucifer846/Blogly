b35b37e595399f71e950f34d427924334f3f362a
saurabh <kapanarinsal6@gmail.com>
2024-12-12 00:49:01 +0530

    time pass commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -12,10 +12,23 @@ res.render('create-blog')
 router.post('/', (req, res)=>{
     const blog = new Blog(req.body)
 
-    blog.save()
+    blog.save()     
     .then(result=>{
         res.redirect('/blogs')
     })
+})
+router.delete('/:id', (req,res)=>{
+    Blog.findByIdAndDelete(req.params.id)
+        .then(result=>{
+            res.json({redirect:'/'})
+        })
+})
+router.get('/:id', (req,res)=>{
+    const id = req.params.id
+    Blog.findById(id)
+        .then(result=>{
+            res.render('details', {blog:result})
+        }).catch
 })
 
 
